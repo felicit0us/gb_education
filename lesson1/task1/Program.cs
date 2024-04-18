c48d8c4fefb8178ef7274de3074615a108de3e1b
felicit0us <inc0.elite@yahoo.com>
2024-04-18 14:35:27 +0300

    add some variables lesson1:task1

--- lesson1/task1/Program.cs
+++ lesson1/task1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Hello, World!");
+
+
+int a = 100;
+bool b = true;
+char c = '!';
+double d = 6.96;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
